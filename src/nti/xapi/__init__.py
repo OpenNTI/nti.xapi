6f0b3840ebba9c88f5baaea1d15ea8f653840427
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-02 15:08:03 -0500

    update headers

--- src/nti/xapi/__init__.py
+++ src/nti/xapi/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
