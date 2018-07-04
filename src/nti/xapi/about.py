3795425b17e22e9c51283625e87c37421fc81bea
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-03 21:49:29 -0500

    pylint; pep8

--- src/nti/xapi/about.py
+++ src/nti/xapi/about.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import interface
-
-from nti.externalization.interfaces import IInternalObjectIO
 
 from nti.schema.schema import SchemaConfigured
 
 from nti.schema.fieldproperty import createDirectFieldProperties
 
-from .interfaces import IAbout
+from nti.xapi.interfaces import IAbout
+
+logger = __import__('logging').getLogger(__name__)
 
 
 @interface.implementer(IAbout)
 class About(SchemaConfigured):
-
     createDirectFieldProperties(IAbout)
-        
