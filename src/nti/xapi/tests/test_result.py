d30b6d12eb29aab5d0d91b301892bcf232c5e722
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-04 21:49:16 -0500

    use has_property

--- src/nti/xapi/tests/test_result.py
+++ src/nti/xapi/tests/test_result.py
@@ -7,50 +7,53 @@ from __future__ import absolute_import
 
 # pylint: disable=protected-access,too-many-public-methods
 
-from hamcrest import assert_that
 from hamcrest import is_
+from hamcrest import assert_that
+from hamcrest import has_property
 
-import isodate
+from nti.testing.matchers import verifiably_provides
 
 import unittest
 
-from zope.schema.interfaces import ValidationError
-
-from nti.testing.matchers import verifiably_provides
+import isodate
 
 from nti.externalization.internalization import update_from_external_object
 
 from nti.externalization.externalization import to_external_object
 
-from . import SharedConfiguringTestLayer
+from nti.xapi.interfaces import IScore
+from nti.xapi.interfaces import IResult
 
-from ..interfaces import IResult
-from ..interfaces import IScore
+from nti.xapi.result import Score
+from nti.xapi.result import Result
 
-from ..result import Result
-from ..result import Score
+from nti.xapi.tests import SharedConfiguringTestLayer
+
 
 class TestScore(unittest.TestCase):
 
     layer = SharedConfiguringTestLayer
 
     def setUp(self):
-
         self.data = {
-        "scaled": 0.95,
+            "scaled": 0.95,
             "raw": 95,
             "min": 0,
             "max": 100
-    }
+        }
         self.score = IScore(self.data)
 
     def validate_score(self, score):
         assert_that(score, verifiably_provides(IScore))
-        assert_that(score.scaled, is_(.95))
-        assert_that(score.raw, is_(95))
-        assert_that(score.min, is_(0))
-        assert_that(score.max, is_(100))
-                
+        assert_that(score,
+                    has_property('scaled', is_(.95)))
+        assert_that(score,
+                    has_property('raw', is_(95)))
+        assert_that(score,
+                    has_property('min', is_(0)))
+        assert_that(score,
+                    has_property('max', is_(100)))
+
     def test_creation(self):
         self.validate_score(self.score)
 
@@ -60,8 +63,8 @@ class TestScore(unittest.TestCase):
     def test_internalization(self):
         score = Score()
         update_from_external_object(score, self.data)
+        self.validate_score(self.score)
 
-        self.validate_score(score)
 
 class TestResult(TestScore):
 
@@ -69,24 +72,27 @@ class TestResult(TestScore):
 
     def setUp(self):
         self.data = {
-        "score":{
-            "scaled": 0.95,
+            "score": {
+                "scaled": 0.95,
                 "raw": 95,
                 "min": 0,
                 "max": 100
-        },
-        "success": True,
-        "completion": True,
-        "duration": "PT1234S"
-    }
+            },
+            "success": True,
+            "completion": True,
+            "duration": "PT1234S"
+        }
         self.result = IResult(dict(self.data))
 
     def validate_result(self, result):
         assert_that(result, verifiably_provides(IResult))
-        assert_that(result.success, is_(True))
-        assert_that(result.completion, is_(True))
-        assert_that(result.duration, is_(isodate.parse_duration('PT1234S')))
-
+        assert_that(result,
+                    has_property('success', is_(True)))
+        assert_that(result,
+                    has_property('completion', is_(True)))
+        assert_that(result,
+                    has_property('duration', 
+                                 is_(isodate.parse_duration('PT1234S'))))
         self.validate_score(result.score)
 
     def test_creation(self):
@@ -99,12 +105,12 @@ class TestResult(TestScore):
         external = to_external_object(self.result)
         external_duration = external.pop('duration')
 
-        assert_that(isodate.parse_duration(external_duration), is_(isodate.parse_duration(expected_duration)))
-    
+        assert_that(isodate.parse_duration(external_duration),
+                    is_(isodate.parse_duration(expected_duration)))
+
         assert_that(external, is_(expected))
 
     def test_internalization(self):
         result = Result()
         update_from_external_object(result, self.data)
-
         self.validate_result(result)
