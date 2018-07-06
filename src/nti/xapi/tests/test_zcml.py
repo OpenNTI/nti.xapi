b7c45d54db96b5d5dbd04f688bd07555ba8326bd
cutz <christopher.m.utz@gmail.com>
2018-07-05 20:40:42 -0500

    zcml for registring lrs client

---
+++ src/nti/xapi/tests/test_zcml.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=protected-access,too-many-public-methods
+
+from hamcrest import is_
+from hamcrest import assert_that
+from hamcrest import has_property
+
+from nti.testing.matchers import verifiably_provides
+
+from zope import component
+
+from nti.xapi.interfaces import ILRSClient
+
+import nti.testing.base
+
+LRS_ZCML_STRING = u"""
+<configure xmlns="http://namespaces.zope.org/zope"
+	xmlns:zcml="http://namespaces.zope.org/zcml"
+	xmlns:xapi="http://nextthought.com/ntp/xapi"
+	i18n_domain='nti.dataserver'>
+	<include package="zope.component" />
+	
+	<include package="." file="meta.zcml" />
+	<xapi:registerLRSClient endpoint="http://nextthought.com/lrs" username="foo" password="bar" />
+</configure>
+"""
+
+
+class TestZcml(nti.testing.base.ConfiguringTestBase):
+
+    def test_lrs(self):
+        self.configure_string(LRS_ZCML_STRING)
+        lrs_client = component.getUtility(ILRSClient)
+        assert_that(lrs_client, has_property('endpoint', 'http://nextthought.com/lrs'))
+        assert_that(lrs_client, has_property('username', 'foo'))
+        assert_that(lrs_client, has_property('password', 'bar'))
+        assert_that(lrs_client, has_property('version', '1.0.3'))
---
+++ src/nti/xapi/zcml.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=inherit-non-class
+
+from functools import partial
+
+from zope import interface
+
+from zope.component.zcml import utility
+
+from zope.configuration import fields
+
+from nti.xapi.client import LRSClient
+
+from nti.xapi.interfaces import ILRSClient
+from nti.xapi.interfaces import Version
+
+logger = __import__('logging').getLogger(__name__)
+
+
+class IRegisterLRSClient(interface.Interface):
+
+    endpoint = fields.TextLine(title=u'The xAPI LRS endpoint',
+                               required=True)
+
+    username = fields.TextLine(title=u'The xAPI LRS username.',
+                               required=False)
+
+    password = fields.TextLine(title=u'client identifier',
+                               required=False)
+
+    version = fields.TextLine(title=u'The SCORM Cloud service URL.',
+                              required=False,
+                              default=Version.latest)
+
+def registerLRSClient(_context, endpoint=None, username=None, password=None, version=Version.latest):
+    factory = partial(LRSClient, endpoint, username=username, password=password, version=version)
+    utility(_context, provides=ILRSClient, factory=factory)
